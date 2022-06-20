429db357cb5399ee35c1a288899518f5dd3ee533
zgs3 <zkairaitis@gmail.com>
2022-06-20 15:56:23 +0300

    Updated readme.md. Added MySQL db file and a favicon.

--- README.md
+++ README.md
@@ -5,3 +5,69 @@
 Also the user is able to add new employees, projects and edit or delete them and all those changes are updated in MySQL database accordingly.
 
 * Project was done as a Sprint 7 task while I was studying at Baltic Institute of Technology. 
+
+
+## Getting Started
+
+* To be able to use the app, you must first have "Apache" server application like "XAMPP" or similar web server app. You can find more info about "XAMPP" [here](https://www.apachefriends.org/).
+
+* Also you will need a tool to manage MySQL database files like "MySQL Workbench". You can find more info about it [here](https://www.mysql.com/products/workbench/).
+
+If you are using XAMPP and MySQL Workbench:
+
+1. Clone this repository to .../xammp/htdocs/ directory (or download the files manually).
+
+2. Open MySQL Workbench, create a new connection and open it. 
+
+Note that in this project default connection username "root" and no password is used. If you intend to use other connection credentials, you must change the username and password in "sql_connection.php" file accordingly.
+
+
+2. 1. Click "Server".
+
+2. 2. Click "Data Import".
+
+2. 3. Choose "Import from Self-Contained File."
+
+2. 4. Provide the "projects.sql" file directory (Located in the "db" folder in cloned repository).
+
+2. 5. Click "new" and type "projects_db" schema name.
+
+Note that schema name must be "projects_db", because this name is written in the code, so the app would not work if the name was different.
+
+2. 6. Click "Start Import".
+
+
+3. Start "Apache" server in "XAMMP".
+
+4. Open your prefered browser and go to localhost/Project_management_php/
+
+Note that the app name in the URL must be the same as the directory name where "index.php" file is located. If directory name is changed, change URL accordingly. 
+
+5. Lastly, log in using given user name and password:
+
+  User name: Manager
+
+  Password: 1234
+
+
+## Description
+
+* The app is connecting to MySQL database and fetching all the data from 2 separate data tables. 
+
+* The user is able to create, edit and delete data.
+
+* This app is password protected, so the user must first log in to be able to use the app. 
+
+
+## Techniques used
+
+Code is written in PHP.
+
+Styled using raw CSS.
+
+
+## Authors
+
+Project made by me - Žygimantas Kairaitis. 
+
+Find me on [LinkedIn](https://www.linkedin.com/in/%C5%BEygimantas-kairaitis-018a86193/).
---
+++ db/projects.sql
@@ -0,0 +1,80 @@
+-- MariaDB dump 10.19  Distrib 10.4.24-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: projects_db
+-- ------------------------------------------------------
+-- Server version	10.4.24-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employees` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(30) COLLATE utf8mb4_lithuanian_ci NOT NULL,
+  `lastName` varchar(30) COLLATE utf8mb4_lithuanian_ci NOT NULL,
+  `assignedProjectId` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `assignedProjectId` (`assignedProjectId`),
+  CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`assignedProjectId`) REFERENCES `projects` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_lithuanian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employees`
+--
+
+LOCK TABLES `employees` WRITE;
+/*!40000 ALTER TABLE `employees` DISABLE KEYS */;
+INSERT INTO `employees` VALUES (1,'Jonas','Petraitis',1),(2,'Bronius','Šaulys',1),(3,'Petras','Kenedis',0),(4,'Benas','Pavardenis',1),(5,'Onutė','Raudonkepuraitė',2),(6,'Angelė','Žemaitė',0),(7,'Joe','Doe',3),(8,'Tomas','Romas',0),(9,'Julius','Baronas',2);
+/*!40000 ALTER TABLE `employees` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projects`
+--
+
+DROP TABLE IF EXISTS `projects`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projects` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `projectName` varchar(30) COLLATE utf8mb4_lithuanian_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `projectName` (`projectName`)
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_lithuanian_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projects`
+--
+
+LOCK TABLES `projects` WRITE;
+/*!40000 ALTER TABLE `projects` DISABLE KEYS */;
+INSERT INTO `projects` VALUES (0,''),(2,'Car shop clients DB'),(3,'Game dev.'),(4,'Online casino dev.'),(1,'Pharmacy e.commerce');
+/*!40000 ALTER TABLE `projects` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-20 10:54:23
